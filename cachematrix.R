fc13d4184a0f46ca3cbe01bade866dcc76647087
Victor Borrero <victorborrero@gmail.com>
2014-09-21 12:23:10 +0200

    Programming Assignment 2 for Victor Borrero

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is programming assignment number 2 for R Programming class
 
-## Write a short comment describing this function
+## This function will cache the inverse of a matrix and return a list
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  
 }
 
 
-## Write a short comment describing this function
+## This function will take the result of the makeCacheMatrix
+## and it will return the mean from cache if it has been stored there
+## or will calculate it if it has not
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+  
 }
